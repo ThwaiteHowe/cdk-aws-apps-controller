aebc6896d81f2c3ea82e2401e9036feb84229fa8
nylespham <nylespham@gmail.com>
2025-06-02 22:46:38 +0700

    THW-11 Change cmd

--- bin/dev.ts
+++ bin/dev.ts
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { CdkInfraControllerStack } from '../lib/cdk-aws-apps-controller-stack';
+import * as dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = new cdk.App();
 new CdkInfraControllerStack(app, 'CdkInfraControllerStack-Dev', {
-  domainName: 'dev.api.thwaitehowe.com',
-  apiDomainName: 'dev.api.thwaitehowe.com',
-  environment: 'dev',
-  apiId: 'kztm729nk5',
+  domainName: process.env.DOMAIN_NAME || 'dev.api.thwaitehowe.com',
+  apiDomainName: process.env.API_DOMAIN_NAME || 'dev.api.thwaitehowe.com',
+  environment: process.env.ENVIRONMENT || 'dev',
+  apiId: process.env.API_ID || 'kztm729nk5',
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT || '1430118840082', // Replace with your AWS account ID
     region: process.env.CDK_DEFAULT_REGION || 'us-east-1', // Replace with your desired region
